e1686ec3fe550616d9d7673d473594e00b3295b6
Exero Ltd <kkurtajov@gmail.com>
2023-11-05 15:51:29 +0200

    Lecture 08 examples

---
+++ src/lecture08/examples/abstraction/abstractclass/Pig.java
@@ -0,0 +1,8 @@
+package lecture08.examples.abstraction.abstractclass;
+
+public class Pig extends Animal {
+    public void animalSound() {
+        // The body of animalSound() is provided here
+        System.out.println("The pig says: wee wee");
+    }
+}
---
+++ src/lecture08/examples/abstraction/interfaceexample/Examples.java
@@ -0,0 +1,9 @@
+package lecture08.examples.abstraction.interfaceexample;
+
+public class Examples {
+    public static void main(String[] args) {
+        Pig myPig = new Pig();  // Create a Pig object
+        myPig.animalSound();
+        myPig.sleep();
+    }
+}
